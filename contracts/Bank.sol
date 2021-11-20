6b133284772057764218d3824111427b6276593c
"bndcts" <"Benedikt_1212@outlook.de">
2021-11-20 11:26:23 +0100

    deposit and withdraw and computeInterest

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -37,10 +37,10 @@ contract Bank is IBank{
      * @return - true if the deposit was successful, otherwise revert.
      */
     function deposit(address token, uint256 amount) payable external override returns (bool){
-        require(msg.value == amount);
-        require(msg.value > 0);
+        require(msg.value == amount, "Message value and amount not the same");
+        require(msg.value > 0, "Value is 0");
              // Ensure sending is to valid address! 0x0 address cane be used to burn() 
-        require(token != address(0));
+        require(token != address(0), "Burn address used");
         accounts[msg.sender].interest = computeInterest(msg.sender);
         accounts[msg.sender].lastInterestBlock = block.number;
         balance[msg.sender] += amount;
@@ -63,12 +63,13 @@ contract Bank is IBank{
      */
     function withdraw(address token, uint256 amount) external override returns (uint256){
         uint256 interest = computeInterest(msg.sender); 
-        require(amount <= accounts[msg.sender].deposit + interest);
+        require(amount <= accounts[msg.sender].deposit + interest, "No sufficient funds");
         accounts[msg.sender].interest = computeInterest(msg.sender);
         accounts[msg.sender].lastInterestBlock = block.number;
+        accounts[msg.sender].deposit -= amount;
         balance[msg.sender] -= amount;
-        payable(token).transfer(amount);
-        return accounts[msg.sender].deposit + accounts[msg.sender].interest;
+        msg.sender.transfer(amount);
+        return amount + accounts[msg.sender].interest;
     }
       
     /**
